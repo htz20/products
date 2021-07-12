6465be041465107c38863c97406eaba4ec1da4d3
jen <leungjen18@gmail.com>
2021-07-12 12:19:38 +0800

    add subtitles

--- products.py
+++ products.py
@@ -3,8 +3,13 @@ while True:
 	name = input ('請輸入商品名稱: ')
 	if name == 'q':
 		break
-	price = input ('請輸入商品價格:')
+	price = input ('請輸入商品價格: ')
 	products.append([name,price])
-
 print (products)
+
+with open ('products.csv', 'w', encoding = 'utf-8') as f:
+	f.write('商品, 價格\n')
+	for p in products:
+		f.write (p[0] + ',' + p[1] + '\n')
+		
 
